0c6e0849a35f6a7f243c26916b3293a21fdcb9f6
Walid Berrouk <jw_berrouk@esi.dz>
2022-10-28 14:59:54 +0100

    Integrating Prodcuts List

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,73 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+// Assets
+import { baseUrl } from '../../shared/baseUrl'
 
 // Assets
 import ProductImage from '../../assets/img/image-cap.svg'
 
 export default function ProductsList() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios
+        .get(`${baseUrl}/products`)
+        .then(res => setProducts(res.data))
+    }, [])
+
+    const productsList = products.map(product => (
+        <div key={product._id} className="card" style={{width: "18rem"}}>
+        <img src={product.image ? product.image : ProductImage} className="card-img-top" alt="Product Image" />
+        <div className="card-body">
+            <h5 className="card-title">{product.name}</h5>
+            <p className="card-text">{product.description}</p>
+            <a href={product.link} className="btn btn-primary">Product Line</a>
+        </div>
+        </div>
+    ))
+
   return (
     <section className='container d-flex gap-3 flex-wrap'>
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
-
-        <div className="card" style={{width: "18rem"}}>
-        <img src={ProductImage} className="card-img-top" alt="Product Image" />
-        <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text">Product Description</p>
-            <a href="#" className="btn btn-primary">Product Line</a>
-        </div>
-        </div>
+        {productsList}
     </section>
   )
 }
